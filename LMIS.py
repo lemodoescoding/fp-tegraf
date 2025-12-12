32ce8443504803366df16a9d3ca8744dd5511238
freddskii <alfred.haryanto.2006@gmail.com>
2025-12-12 21:19:23 +0700

    updating to py

---
+++ LMIS.py
@@ -0,0 +1,41 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+def buildTree(arr):
+    root = TreeNode(None)
+    for i in range(len(arr)):
+        child = TreeNode(arr[i])
+        root.children.append(child)
+        buildSubtree(arr, i + 1, arr[i], child)
+    return root
+
+def buildSubtree(arr, index, last_value, parent):
+    for i in range(index, len(arr)):
+        if arr[i] > last_value:
+            child = TreeNode(arr[i])
+            parent.children.append(child)
+            buildSubtree(arr, i + 1, arr[i], child)
+
+def dfs(node, path, all_paths):
+    if node.value is not None:
+        path.append(node.value)
+    if not node.children:
+        if len(path) > 0:
+            all_paths.append(path.copy())
+    else:
+        for child in node.children:
+            dfs(child, path, all_paths)
+    if node.value is not None:
+        path.pop()
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+root = buildTree(arr)
+all_paths = []
+dfs(root, [], all_paths)
+longest = max(all_paths, key=len)
+
+print(longest)
